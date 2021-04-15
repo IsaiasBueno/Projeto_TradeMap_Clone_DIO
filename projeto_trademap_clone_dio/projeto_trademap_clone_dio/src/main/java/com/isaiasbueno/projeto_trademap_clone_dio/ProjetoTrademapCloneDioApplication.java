2068105663a20c03c7622ff70440e47e1b2d5a4d
IsaiasBueno <izaias.bueno@gmail.com>
2021-04-15 10:50:39 -0300

    Update/Commit 15/04/2021

---
+++ projeto_trademap_clone_dio/projeto_trademap_clone_dio/src/main/java/com/isaiasbueno/projeto_trademap_clone_dio/ProjetoTrademapCloneDioApplication.java
@@ -0,0 +1,13 @@
+package com.isaiasbueno.projeto_trademap_clone_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTrademapCloneDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTrademapCloneDioApplication.class, args);
+	}
+
+}
---
+++ projeto_trademap_clone_dio/projeto_trademap_clone_dio/src/test/java/com/isaiasbueno/projeto_trademap_clone_dio/ProjetoTrademapCloneDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiasbueno.projeto_trademap_clone_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTrademapCloneDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
